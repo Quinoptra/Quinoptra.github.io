4e82f2ff9a1d8fbd5b9dec976b5d4450a7dd982e
Angus Huang <92761019+Quinoptra@users.noreply.github.com>
2021-10-22 11:32:41 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-var name = document.getElementById('username').value
-function saveuser(){
-  window.alert(name)
-}
-function callingVar(){
-  window.alert(name)
-}
-
 var a;
 function getName(){
   //get value from input field and save as "a"
    a = document.getElementById('username').value;
+  window.alert(a)
   //create LS key with value of "a"
    localStorage.setItem('uName', a);
   //automatically take the user to page two.html
