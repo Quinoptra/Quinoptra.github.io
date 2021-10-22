3a64e60786e596605789956350fd5b873d52cac4
Angus Huang <92761019+Quinoptra@users.noreply.github.com>
2021-10-22 11:23:25 -1000

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function saveuser(){
 function callingVar(){
   window.alert(name)
 }
+
+var a;
+function getName(){
+  //get value from input field and save as "a"
+   a = document.getElementById('username').value;
+  //create LS key with value of "a"
+   localStorage.setItem('uName', a);
+  //automatically take the user to page two.html
+  location.href = "two.html"
+}
