4cbcc8c2c34f643e50750b89d42ab25cccd3ee2f
Angus Huang <92761019+Quinoptra@users.noreply.github.com>
2021-10-22 11:42:30 -1000

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ var a;
 function getName(){
   //get value from input field and save as "a"
    a = document.getElementById('username').value;
-  window.alert(a)
   //create LS key with value of "a"
-   localStorage.setItem('uName', a);
+  localStorage.setItem('uName', a);
   //automatically take the user to page two.html
   location.href = "two.html"
   window.alert(a)
